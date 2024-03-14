0095d91c96bdc7c88cc68000eb86b98ed29914c1
InFlames <85372327+InFlames513@users.noreply.github.com>
2024-03-14 17:50:03 +0300

    Update date.js

--- src/date.js
+++ src/date.js
@@ -1,3 +1,3 @@
 let [date, days, months] = [new Date(), ["Pazar", "Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartesi"], ["Ocak", "Şubat", "Mart", "Nisan", "Mayıs", "Haziran", "Temmuz", "Ağustos", "Eylül", "Ekim", "Kasım", "Aralık"]];
 document.getElementById("date").innerHTML=`${date.getDate()} ${months[date.getMonth()]} ${days[date.getDay()]}`
-document.getElementById("day").innerHTML=`(${Math.floor((new Date().getTime() - new Date().setFullYear(2024, 3, 11))/(1000*60*60*24))}. Gün)`
+document.getElementById("day").innerHTML=`(${Math.floor((new Date().getTime() - new Date().setFullYear(2024, 2, 9))/(1000*60*60*24))}. Gün)`
